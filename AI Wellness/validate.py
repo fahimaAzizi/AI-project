4c5cf0658c6352bbaa8ad455bf9e5d1c82d12f3b
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2025-04-19 21:08:45 +0500

    Update validate.py

--- AI Wellness/validate.py
+++ AI Wellness/validate.py
@@ -10,8 +10,25 @@
 # daf hello(req: add):
 #   return "helloMr."
 import pyttsx3
+import speech_recognition as sr  # use 'sr' for shorter reference
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
-engine.setProperty("voice",voices[0].id)
-print(voices[0])
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+
+
+# Example usage
+speak("Hello! How can I help you?")
+takeCommand()
+
+
+
+
+ 
