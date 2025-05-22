8278316ce30b97159f1887736b49679150b9b50e
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2025-05-22 20:56:28 +0500

    Update focuseGraph.py

---
+++ AI Wellness/focuseGraph.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as pt
+
+def focus_graph():
+    file = open("focus.txt","r")
+    content = file.read()
+    file.close()
+   
+   
+   
+    content =content.split(",")
+    x1 =[]
+    for i in range(0,len(content)):
+        content[i] = float(content[i])
+        x1.append(i)
+
+        print(content)
+        y1 = content
+
+        pt.plot(x1,y1,color = "red" , marker ="o")
+        pt.title("YOUR FOCUS TIME",fontsize = 16)
+        pt.xlabel("Time",fontsize =14)
+        pt.ylabel("Focus time" ,fontsize =14)
+        pt.grid()
+        pt.show()
